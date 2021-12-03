e115593d145a689d3a129724ce92d88b1ed63e6f
AslanSN <alan.staub@hotmail.com>
2021-12-03 10:18:10 +0000

    Excessive commenting deleted

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
-//import react into the bundle
+
 import React from "react";
 import ReactDOM from "react-dom";
-
-//include bootstrap npm library into the bundle
 import "bootstrap";
 
-//include your index.scss file into the bundle
+
+//Comonents
+import Home from "./component/Home.jsx";
+
+//Styles
 import "../styles/index.scss";
 
-//import your own components
-import Home from "./component/home.jsx";
 
-//render your react application
+//Rendering
 ReactDOM.render(<Home />, document.querySelector("#app"));
